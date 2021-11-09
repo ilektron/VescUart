99dfe51bac8e4993920d834bd247e648cb9e2189
Stephen Farnsworth <robotoman@gmail.com>
2021-11-08 20:46:38 -0700

    Started cleaning up to be header only library

---
+++ test/test.cpp
@@ -0,0 +1,60 @@
+#include "../src/buffer.h"
+#include <cstdint>
+
+bool test_append_uint8_t()
+{
+    buffer<uint8_t, 8> b;
+    b.append<uint8_t>(5);
+
+}
+
+bool test_append_uint16_t()
+{
+
+}
+
+bool test_append_uint32_t()
+{
+
+}
+
+bool test_append_uint64_t()
+{
+
+}
+
+bool test_append_int8_t()
+{
+
+}
+
+bool test_append_int16_t()
+{
+
+}
+
+bool test_append_int32_t()
+{
+
+}
+
+bool test_append_int64_t()
+{
+
+}
+
+bool test_append_float16()
+{
+
+}
+
+bool test_append_float32()
+{
+
+}
+
+
+int main()
+{
+
+}
